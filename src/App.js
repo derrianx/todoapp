76bba12a8639a9d118e1dba347286f18cb9b4118
derrianx <derrianchia001@suss.edu.sg>
2023-03-19 15:08:32 +0800

    adding the app files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// for semantic ui
+import { Button, Input, List } from "semantic-ui-react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function TodoList() {
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const handleDeleteTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  return (
+    <div className="MyTasksPage-list">
+      <TaskForm onAddTask={handleAddTask} />
+      <TaskList tasks={tasks} onDeleteTask={handleDeleteTask} />
+    </div>
+  );
+}
+
+function TaskForm({ onAddTask }) {
+  const [task, setTask] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!task) {
+      return;
+    }
+
+    const newTask = { id: Date.now(), title: task };
+    onAddTask(newTask);
+    setTask("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Input
+        type="text"
+        value={task}
+        onChange={(event) => setTask(event.target.value)}
+        placeholder="Add a task"
+      />
+      <Button class="ui loading button" type="submit" primary>
+        <i className="plus icon"></i>
+        Add Task
+      </Button>
+      <button class="ui facebook button">
+        <i class="facebook icon"></i>
+        Facebook
+      </button>
+    </form>
+  );
+}
+
+function TaskList({ tasks, onDeleteTask }) {
+  return (
+    <List>
+      {tasks.map((task) => (
+        <List.Item key={task.id}>
+          <List.Content floated="left">{task.title}</List.Content>
+          <List.Content floated="right">
+            <Button onClick={() => onDeleteTask(task.id)} negative>
+              Delete
+            </Button>
+          </List.Content>
+        </List.Item>
+      ))}
+    </List>
+  );
+}
 
 function App() {
   return (
@@ -7,7 +81,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Hi
         </p>
         <a
           className="App-link"
@@ -17,6 +91,7 @@ function App() {
         >
           Learn React
         </a>
+        <TodoList />
       </header>
     </div>
   );
